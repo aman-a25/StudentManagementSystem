f1dafabb60c4648ddab885d7675ede4b040b2384
Aman kumar <ak9910450375@gmail.com>
2025-12-19 21:44:03 +0530

    addcourse function in CourseHandler complete final

--- src/CourseHandler.java
+++ src/CourseHandler.java
@@ -10,7 +10,49 @@ public class CourseHandler implements CourseOperations {
 
     @Override
     public void addCourse() {
-        // we implement next
+        System.out.println("\n ====== REGISTER NEW COURSE ======");
+
+        String name ;
+
+        System.out.println("Enter the name of the new course: ");
+
+        // Force valid course name
+        while (true) {
+            System.out.print("Enter the name of the new course: ");
+            name = scan.nextLine().trim();
+
+            if (!name.isEmpty()) {
+                break;
+            }
+            System.out.println("❌ Course name cannot be empty. Please try again.");
+        }
+        String sql = "INSERT INTO courses (course_name) VALUES (?)";
+
+        try (
+                Connection con = DbConnection.getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)
+        ){
+
+            // preparing the statement with the pre created sql string
+            ps.setString(1, name);
+
+            // executing the prepared statement
+            int rows = ps.executeUpdate();
+
+            if (rows > 0) {
+                System.out.println("✅ course added successfully!");
+            } else {
+                System.out.println("❌ Failed to add course.");
+            }
+        } catch (SQLException e) {
+
+            if (e.getMessage().toLowerCase().contains("duplicate")) {
+                System.out.println("❌ This course already exists.");
+            } else {
+                System.out.println("❌ Error adding course: " + e.getMessage());
+            }
+        }
+
     }
 
     @Override
